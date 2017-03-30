73e9861034bd44a2535d193fa42688c620ae575b
Kang Jiawei <kangjiawei@pimpin.cn>
2017-03-30 09:48:27 +0800

    migration 后端登陆模块

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,29 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::any('api',function(){
+    return ['version'=>0.1,'author'=>'James Kang'];
+});
+
+Route::any('api/signup',function(){
+    $user=new App\User;
+    return $user->signup();
+});
+
+Route::any('api/login',function(){
+    $user=new App\User;
+    return $user->login();
+});
+
+Route::any('api/logout',function(){
+    $user=new App\User;
+    return $user->logout();
+});
+
+Route::any('test',function(){
+    $user=new App\User;
+    dd($user->is_logged_in());
+});
+
+
